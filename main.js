f28674deb2e0d73331ab6f693b972d4254be7324
Ben Hedgepeth <benhh@gmx.com>
2021-02-01 22:18:36 -0800

    Formatted y-axis ticks

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ request += "ProjectReferenceData/master/cyclist-data.json";
 
 const margin = {
   top: 20,
-  right: 30,
+  right: 60,
   bottom: 20,
-  left: 20
+  left: 60
 };
 
-const width = 800;
+const width = 950;
 const innerWidth = width - (margin.left + margin.right);
 const height = 575;
 const innerHeight = height - (margin.top + margin.bottom);
@@ -25,33 +25,37 @@ d3.json(request).then((response) => {
   let years = response.map((x) => new Date(x.Year, 0));
   let times = years.map((year, i) => {
     let [minutes, seconds] = response[i].Time.split(":");
-    return new Date(year.getUTCFullYear(), 0, 1, 0, +minutes, +seconds);
+    return new Date(0, 0, 1, 0, +minutes, +seconds); // passing year causes y-axis render to break
   });
 
 
   // Scales
   let xScale = d3.scaleTime(
-    d3.extent(years, (d) => d.getFullYear()),
+    d3.extent(years),
     [0, innerWidth]
   );
-
+  console.log(xScale.domain())
   let yScale = d3.scaleTime(
-    d3.extent(response, (d) => d.Seconds),
-    [innerHeight, 0]
+    d3.extent(times),
+    [0, innerHeight]
   );
+
+  console.log(yScale.domain())
+
+  // Axes
+  let xAxis = d3.axisBottom(xScale);
+  let yAxis = d3.axisLeft(yScale)
+                .tickFormat(d3.timeFormat("%M:%S"));
 
   // Data plotting
   gCircles.selectAll("circles")
     .data(response).enter().append("circle")
-    .attr("cx", (d) => xScale(d.Year))
-    .attr("cy", (d) => innerHeight - yScale(d.Seconds))
-    .attr("r", 7)
+    .attr("cx", (d, i) => xScale(years[i]))
+    .attr("cy", (d, i) => yScale(times[i]))
+    .attr("r", 7);
 
-  // Axes
-  // let xAxis = d3.axisBottom(xScale);
-  // let yAxis = d3.axisLeft(yAxis);
-  //
-  // xAxis(gAxes.append("g").attr("transform", `translate(${width / 2}, ${height - margin.bottom})`));
 
+  xAxis(svg.append("g").attr("transform", `translate(${margin.left}, ${height - margin.bottom})`));
+  yAxis(svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`));
 
 })
